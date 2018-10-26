e03062ed829f8d080157099a34626800715eedd8
juanjorgegarcia <juan.j.garcia007@gmail.com>
2018-10-26 11:38:39 -0300

    receber consertado

--- Projeto8-AM/receive.py
+++ Projeto8-AM/receive.py
@@ -5,29 +5,42 @@ import matplotlib.pyplot as plt
 import sounddevice as sd
 time = 0.5
 amplitude = 1
-freq = 8000
+freq = 12000
 fs = 44100
-duration = 2
+duration = 5
 
 sig = st()
+print("gravando")
 myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
 sd.wait()
+print("pronto")
+myrecording = [i[0] for i in myrecording]
 
-myrecording = [i[0]for i in myrecording]
-
+#myrecording, samplerate = sf.read('test.wav')
 sd.play(myrecording)
+sd.wait()
 
-x,carrier = sig.generateSin(freq,amplitude,65621/fs,fs)
+sig.plotFFT(myrecording, fs)
+plt.show()
+
+x,carrier = sig.generateSin(freq,amplitude,len(myrecording)/fs,fs)
 demodule_audio = list(map(lambda x,y: (x)*y, myrecording,carrier))
+sig.plotFFT(demodule_audio, fs)
+plt.show()
 
-#exemplo de filtragem do sinal yAudioNormalizado
-# https://scipy.github.io/old-wiki/pages/Cookbook/FIRFilter.html
-nyq_rate = fs/2
-width = 5.0/nyq_rate
+a = [0 for i in range(20000)]
+audio = demodule_audio+a
+
+taxa = fs/2
+width = 5.0/taxa
 ripple_db = 60.0 #dB
 N , beta = signal.kaiserord(ripple_db, width)
 cutoff_hz = 4000.0
-taps = signal.firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
-filtered_audio = signal.lfilter(taps, 1.0, myrecording)
-sd.play(filtered_audio)
+taps = signal.firwin(N, cutoff_hz/taxa, window=('kaiser', beta))
+yFiltrado = signal.lfilter(taps, 1.0, demodule_audio)
+
+sig.plotFFT(yFiltrado, fs)
+plt.show()
+print("audio filtrado")
+sd.play(yFiltrado)
 sd.wait()
